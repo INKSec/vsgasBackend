b7597c235a92926700e7fb900cd6d440e0742327
Sascha Usison <usisonsa@LDED5109.corp.draeger.global>
2022-05-14 15:35:04 +0200

    Init PR

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const db = require("./app/models");
+db.sequelize.sync();
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+app.use(cors(corsOptions));
+// parse requests of content-type - application/json
+app.use(express.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to VSGas" });
+});
+// set port, listen for requests
+const PORT = process.env.PORT || 8888;
+
+require("./app/routes/tutorial.routes")(app);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
